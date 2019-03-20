45326266d7d242bf03173fe9496a2de5ae619049
JulianYanez <julianf.yanez@gmail.com>
2019-03-20 15:07:34 -0500

    First Working Artist Call

---
+++ README.md
@@ -0,0 +1,3 @@
+# Testing the Spotify Web API with Spotipy Wrapper
+
+## Will obtain information about the type of music I listen to using my user profile data and data about the kinds of songs/albums I find most enjoyable.
--- spotify.py
+++ spotify.py
@@ -1,8 +1,19 @@
 # pip install spotipy
+import spotipy
+from api_keys import client_id, client_secret
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+client_credentials_manager = SpotifyClientCredentials(client_id,client_secret)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 
 def get_song():
     pass
 def get_album():
     pass
-def get_artist():
-    pass
+def get_artist(name):
+    results = spotify.search(q='artist:' + name, type='artist')
+    return results
+
+get_artist("SZA")
